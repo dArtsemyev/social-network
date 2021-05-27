6d8e7d95f92dc9a91356b9d84ea7d14093713977
dArtsemyev <d.artsemyev@gmail.com>
2021-05-27 19:53:59 +0300

    removed old errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,17 @@
-
-import state, {subscribe} from "./redux/state";
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
-import {addPost, updateNewPostText} from "./redux/state";
 
 let rerenderEntireTree = () => {
     ReactDOM.render(<React.StrictMode>
-            <BrowserRouter>
-                <App state={state}
-                     addPost={addPost}
-                     updateNewPostText={updateNewPostText}
-                />
-            </BrowserRouter>
+
+                <App />
+
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 rerenderEntireTree()
-
-subscribe(rerenderEntireTree)
 
 
