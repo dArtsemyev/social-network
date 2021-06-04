033cb017794ff90fdd125ef8c574a980b6bebb95
dArtsemyev <d.artsemyev@gmail.com>
2021-06-04 08:12:04 +0300

    Created Dialogs component with Dialogs.module.css file. Moved css content style from Profile.module.css to App.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -11,7 +12,10 @@ function App() {
             <Header/>
             <div className="nav-content">
                 <Navbar/>
-                <Profile/>
+                <div className="content">
+                    {/*<Profile/>*/}
+                    <Dialogs/>
+                </div>
             </div>
             <Footer/>
         </div>
