7a20b3f4258203b1d26ae3818fd1f1708e0d036c
dArtsemyev <d.artsemyev@gmail.com>
2021-06-08 20:19:53 +0300

    Dialogs page was created

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,29 @@
 import React from "react"
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css"
 
 function Dialogs() {
     return (
         <div className={s.dialogs}>
-            Dialogs
+            <div className={s.dialogItems}>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/dmitry" activeClassName={s.active}>Dmitry</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/vasya" activeClassName={s.active}>Vasya</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/petya" activeClassName={s.active}>Petya</NavLink>
+                </div>
+                <div className={s.item}>
+                    <NavLink to="/dialogs/kolya" activeClassName={s.active}>Kolya</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>Good by</div>
+            </div>
         </div>
     );
 }
